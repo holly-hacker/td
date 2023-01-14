a8317f1fb10d1e20884d91a6df086a8d14635522
HoLLy <dario.db@hotmail.com>
2023-01-14 14:18:19 +0100

    Use deliberate disk model for database

--- td-lib/src/database/v1/mod.rs
+++ td-lib/src/database/v1/mod.rs
@@ -4,19 +4,128 @@ use petgraph::stable_graph::StableDiGraph;
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
 
-// TODO: maybe want to have separate disk model and in-memory model? currently relying on petgraph's internal structure
-// I can implement that using a custom serialize/deserialize implementation
-#[derive(Debug, Default, Serialize, Deserialize)]
+use crate::utils::generate_unique_id;
+
+/// The in-memory representation of the database
+#[derive(Debug, Clone, Default)]
 pub struct Database {
-    /// The list of tasks in this database.
+    /// The graph of tasks in this database.
     ///
-    /// This uses an IndexMap to keep a stable order, which means insertions and removals in the
-    /// internal database will not cause large changes to the database file.
+    /// This uses a StableDiGraph to keep a stable order, which means insertions and removals will
+    /// not cause large changes to the database file.
     pub tasks: StableDiGraph<Task, TaskDependency>,
+}
+
+impl Serialize for Database {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let model: DatabaseDiskModel = self.clone().into();
+        model.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for Database {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let model = DatabaseDiskModel::deserialize::<D>(deserializer)?;
+        Ok(model.into())
+    }
+}
+
+/// The database model as stored to disk.
+#[derive(Deserialize, Serialize)]
+struct DatabaseDiskModel {
+    tasks: Vec<TaskDiskModel>,
+}
+
+impl From<Database> for DatabaseDiskModel {
+    fn from(value: Database) -> Self {
+        let mut list = vec![];
+
+        // collect nodes
+        for node_idx in value.tasks.node_indices() {
+            let node_weight = value.tasks[node_idx].clone();
+            list.push((node_idx, TaskDiskModel::new(node_weight)));
+        }
+
+        // collect edges
+        for edge_idx in value.tasks.edge_indices() {
+            let (start_index, end_index) = value
+                .tasks
+                .edge_endpoints(edge_idx)
+                .expect("each edge should be connected");
+
+            let end_id = list
+                .iter()
+                .find_map(|x| (x.0 == end_index).then(|| x.1.task.id.clone()))
+                .expect("should be able to find end node");
+            let start_node = list
+                .iter_mut()
+                .find(|x| x.0 == start_index)
+                .expect("should be able to find start node");
+
+            start_node.1.dependencies.push(end_id);
+        }
+
+        Self {
+            tasks: list.into_iter().map(|x| x.1).collect(),
+        }
+    }
+}
+
+impl From<DatabaseDiskModel> for Database {
+    fn from(value: DatabaseDiskModel) -> Self {
+        let mut graph = StableDiGraph::new();
+        let mut id_index_map = vec![];
+
+        // store nodes
+        for task in &value.tasks {
+            let id = task.task.id.clone();
+            let index = graph.add_node(task.task.clone());
+            id_index_map.push((id, index));
+        }
+
+        // store edges
+        for task in &value.tasks {
+            let source_id = task.task.id.clone();
+            for target_id in task.dependencies.iter().cloned() {
+                let source_index = id_index_map.iter().find(|x| x.0 == source_id).unwrap().1;
+                let target_index = id_index_map.iter().find(|x| x.0 == target_id).unwrap().1;
+
+                graph.add_edge(source_index, target_index, TaskDependency::new());
+            }
+        }
+
+        Self { tasks: graph }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct TaskDiskModel {
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    dependencies: Vec<String>,
+
+    #[serde(flatten)]
+    task: Task,
+}
+
+impl TaskDiskModel {
+    pub fn new(task: Task) -> Self {
+        Self {
+            task,
+            dependencies: vec![],
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Task {
+    /// A unique id for this task
+    id: String,
     /// A short description of this task.
     pub title: String,
     /// When the task has been created.
@@ -37,6 +146,7 @@ impl Task {
         let time_created =
             OffsetDateTime::now_local().unwrap_or_else(|_| OffsetDateTime::now_utc());
         Self {
+            id: generate_unique_id(),
             title,
             time_created,
             time_started: None,
@@ -46,7 +156,7 @@ impl Task {
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct TaskDependency {}
 
 impl TaskDependency {
--- td-lib/src/lib.rs
+++ td-lib/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod database;
 pub mod errors;
+pub(crate) mod utils;
 
 pub use petgraph;
 pub use time;
